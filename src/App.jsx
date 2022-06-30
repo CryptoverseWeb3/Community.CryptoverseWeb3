15f52ec5b6161ddef27f7b74cac9bccf1482a79a
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-30 12:11:53 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Homepage from './Pages/Homepage';
+
+const App = () => {
+    return (
+        <>
+            <Homepage/>
+        </>
+    );
+};
+
+export default App;
