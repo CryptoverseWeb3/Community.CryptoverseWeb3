c7d7d263500db54814fe1e1da147bf938a6138e4
missic <missic@pop-os.localdomain>
2022-10-17 18:36:00 +0545

    removing comments

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router-dom";
 
 import "./App.css";
 import Homepage from "./Pages/Homepage";
-// import ProjectsPage from "./Pages/ProjectsPage";
 import { Navbar, Sidebar, RoadmapResources,ViewProject } from "./components";
 
 const App = () => {
